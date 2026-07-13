2fed2c4a184ab011adacffb7571c1963ff7bd063
jinov <jinova95@seznam.cz>
2026-07-13 20:30:36 +0200

    fix: change Account ID generation to UUID and add docker-compose.yml

--- src/main/java/com/jindero/banking/features/account/Account.java
+++ src/main/java/com/jindero/banking/features/account/Account.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 public abstract class Account {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   @Column(unique = true)
